a2a23448ffc81469774794e0379840fbca927b7a
老坛咸鱼 <55818290+xt-9931@users.noreply.github.com>
2021-05-12 14:09:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,27 @@
 
-import jieba
-import wordcloud
-import os
-import imageio
-import matplotlib.pyplot as plt
+import jieba    //jieba分词工具包
+import wordcloud    //词云包
+import os     //获取路径
+import imageio    //图片操作
+import matplotlib.pyplot as plt     //绘图
 
-d = os.path.dirname(__file__)
+d = os.path.dirname(__file__)     //获取当前路径
 
-isCN = 1
-background_image = "2.png"
-font_path1 = "STXINGKA.TTF"
-image1 = "wordcloudimage1.png"
+isCN = 1      //中文分词
+background_image = "2.png"    
+font_path1 = "STXINGKA.TTF"     //路径名
+image1 = "wordcloudimage1.png"    
 image2 = "wordclooudimage2.png"
-back_coloring = imageio.imread(background_image)
+back_coloring = imageio.imread(background_image)    //读取背景图片，设定词云样式
 wca = wordcloud.WordCloud(font_path=font_path1,
                           background_color="white",
                           max_words=200,
                           mask=back_coloring,
                           max_font_size=300,
                           random_state=42,
-                          width=1000, height=860, margin=2)
-file = open("175315.txt",'rb').read()
-seg_list = jieba.cut(file, cut_all=False)
+                          width=1000, height=860, margin=2)       //创建词云对象，
+file = open("175315.txt",'rb').read()     //读取要分析的文本文件
+seg_list = jieba.cut(file, cut_all=False)     //
 seg_list = " ".join(seg_list)
 wca.generate(seg_list)
 image_colors = wordcloud.ImageColorGenerator(back_coloring)
